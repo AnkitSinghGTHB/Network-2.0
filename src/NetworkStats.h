69f9767ad39da23d29ca2e2307e9cc343ea8a1c1
Nitya Gosain <nityagosain@gmail.com>
2025-08-26 00:51:35 +0530

    Create NetworkStats.h

---
+++ src/NetworkStats.h
@@ -0,0 +1,35 @@
+#ifndef NETWORK_STATS_H
+#define NETWORK_STATS_H
+
+#include "PacketTypes.h"
+#include <unordered_map>
+#include <string>
+#include <chrono>
+
+class NetworkStats {
+private:
+    uint64_t totalPackets;
+    uint64_t totalBytes;
+    uint64_t anomalousPackets;
+    
+    std::unordered_map<std::string, uint64_t> protocolCounts;
+    std::unordered_map<std::string, uint64_t> ipCounts;
+    
+    std::chrono::system_clock::time_point startTime;
+    std::chrono::system_clock::time_point lastPacketTime;
+    
+public:
+    NetworkStats();
+    
+    void recordPacket(const PacketInfo& packet);
+    void reset();
+    void printStats() const;
+    void printLiveTable(const std::vector<PacketInfo>& recentPackets) const;
+    
+    uint64_t getTotalPackets() const { return totalPackets; }
+    uint64_t getTotalBytes() const { return totalBytes; }
+    uint64_t getAnomalousPackets() const { return anomalousPackets; }
+    double getPacketsPerSecond() const;
+};
+
+#endif
